1cd02819df51f7680b31306f1bbad0d6dfa1019d
parthibaan416 <parthishankar416@outlook.com>
2025-07-30 11:46:37 +0530

    first commit

---
+++ src/main/java/com/sptraders/sptraders_billing/SptradersBillingApplication.java
@@ -0,0 +1,13 @@
+package com.sptraders.sptraders_billing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptradersBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SptradersBillingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sptraders/sptraders_billing/SptradersBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sptraders.sptraders_billing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SptradersBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
